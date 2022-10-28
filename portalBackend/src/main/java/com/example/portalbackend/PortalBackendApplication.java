c2363ca7331c88e8ee0eb31584a8bb76ae78c32b
trivedidvijen <trivedidvijen@gmail.com>
2022-10-28 18:02:47 -0400

    Initial Setup for Backend SprintBoot Application

---
+++ portalBackend/src/main/java/com/example/portalbackend/PortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.portalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalBackendApplication.class, args);
+	}
+
+}
