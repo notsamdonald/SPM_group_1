75398fb89f246b5ffc80f822ff8adc25be9158b0
trivedidvijen <trivedidvijen@gmail.com>
2022-11-07 13:42:19 -0500

    Added CORS filter for frontend

--- portalBackend/src/main/java/com/example/portalbackend/controllers/FlightController.java
+++ portalBackend/src/main/java/com/example/portalbackend/controllers/FlightController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/flight")
+@CrossOrigin(origins = "*")
 public class FlightController {
     private Logger log = LoggerFactory.getLogger(FlightController.class);
 
